1f23fd4000e8c41e27e5905564eacf9b1318356d
shilpisinghh <sshilpi.mca@gmail.com>
2018-06-28 11:18:22 +0530

    restful webservice using springboot

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/User/UserResource.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/User/UserResource.java
@@ -1,5 +1,8 @@
 package com.rest.webservices.restfulwebservices.User;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.hateoas.Resource;
 import javax.validation.Valid;
 import java.util.List;
 import java.net.URI;
@@ -25,13 +29,16 @@ public class UserResource {
 	}
 	
 	@GetMapping(path="/users/{id}")
-	public User getUser(@PathVariable int id) throws Exception{
+	public Resource<User> getUser(@PathVariable int id) throws Exception{
 		User user=service.findOne(id);
 		if(user==null) {
 			//throw new Exception("testing");
 			throw new UserNotFoundException("testing Customized Exception");
 		}
-		return user;
+		Resource<User> resource=new Resource<User>(user);
+		ControllerLinkBuilder linkTo=linkTo(methodOn(this.getClass()).getAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		return resource;
 	}
 	
 	@PostMapping(path="/users")
