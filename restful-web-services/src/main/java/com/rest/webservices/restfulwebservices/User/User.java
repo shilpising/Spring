daeb5ff768fa33e487d8c81a80172e6798ad86d9
shilpisinghh <sshilpi.mca@gmail.com>
2018-07-05 12:42:51 +0530

    rest+JPA

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/User/User.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/User/User.java
@@ -1,19 +1,30 @@
 package com.rest.webservices.restfulwebservices.User;
 
 import java.util.Date;
+import java.util.List;
 import javax.validation.constraints.Size;
 import javax.validation.constraints.Past;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.OneToMany;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(description="Details about the user")
+@Entity
 public class User {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=2,message="Name should have atleast 2 characters")
 	@ApiModelProperty(notes="Name should have atleast 2 characters")
 	private String name;
+	
+	@OneToMany(mappedBy="user")
+	private List<Post> posts;
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -44,6 +55,12 @@ public class User {
 	}
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
+	}
+	public List<Post> getPosts() {
+		return posts;
+	}
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 	
 
